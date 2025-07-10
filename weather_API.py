c8900290df79a27f4a18dbbd0d4acf6606b91713
abhirup2565 <abhirup2565@gmail.com>
2025-07-10 23:31:26 +0530

    api handeling

--- weather_API.py
+++ weather_API.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask,jsonify,request,redirect,url_for,render_template
+from flask import Flask,request,render_template
 
 
 
@@ -7,13 +7,17 @@ app=Flask(__name__)
 
 @app.route("/",methods=["POST","GET"])
 def home():
+    API_KEY_NEWS=
+    response_news=requests.get(f"https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY_NEWS}")
+    news=response_news.json()
+
     datas={}
     if request.method=="POST":
         city_name=request.form.get("city")
         API_key=
         response=requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}")
         datas=response.json()
-    return render_template("home.html",datas=datas)
+    return render_template("home.html",datas=datas,news=news)
 
 if __name__=="__main__":
     app.run(debug=True)
