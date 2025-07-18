6bf7e9791bafa4745a68de4a2e6265bad6fe9529
abhirup2565 <abhirup2565@gmail.com>
2025-07-18 11:23:26 +0530

    working api to get current data

--- WeatherApp/main.py
+++ WeatherApp/main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from flask import Flask
+from dataclasses import dataclass
 import requests
 import os
 
@@ -12,5 +13,14 @@ def get_lat_lon(city_name,state_code,country_code,API_KEY):
     lat,lon=data.get('lat'),data.get('lon')#lat,lon=data['lat'],data['lon']s
     return lat,lon
 
+def get_current_weather(lat,lon,API_KEY):
+    res=requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}").json()
+    print(res)
 
-print(get_lat_lon("Mumbai","","India",API_KEY))
+@dataclass
+class WeatherData:
+    pass
+
+if __name__=="__main__":
+    lat,lon=get_lat_lon("Mumbai","","India",API_KEY)
+    get_current_weather(lat,lon,API_KEY)
